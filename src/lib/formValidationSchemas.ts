ec41a2ed260500a8d667bd3a98480af372482382
eckintosh <4ai360@gmail.com>
2026-01-21 06:11:41 -0800

    Zod fixed

--- src/lib/formValidationSchemas.ts
+++ src/lib/formValidationSchemas.ts
@@ -9,11 +9,11 @@ export const subjectSchema = z.object({
 export type SubjectSchema = z.infer<typeof subjectSchema>;
 
 export const classSchema = z.object({
-  id: z.coerce.number().optional(),
+  id: z.number().optional(),
   name: z.string().min(1, { message: "Class name is required!" }),
-  capacity: z.coerce.number().min(1, { message: "Capacity is required!" }),
-  gradeId: z.coerce.number().min(1, { message: "Grade is required!" }),
-  supervisorId: z.coerce.string().optional(),
+  capacity: z.number().min(1, { message: "Capacity is required!" }),
+  gradeId: z.number().min(1, { message: "Grade is required!" }),
+  supervisorId: z.string().optional(),
 });
 
 export type ClassSchema = z.infer<typeof classSchema>;
