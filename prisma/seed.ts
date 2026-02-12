cb6809c11e14528ac96bfad78249a287c0126d0a
eckintosh <4ai360@gmail.com>
2026-02-12 10:28:12 +0000

    fixing error for vercel phase 3

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 import bcrypt from "bcryptjs";
+import * as dotenv from "dotenv";
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+const connectionString = `${process.env.DATABASE_URL}`
+const pool = new pg.Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   // Clear existing data to avoid conflicts
